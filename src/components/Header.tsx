c5f7d063a90c8ad0452deebf789b9e79c2aa4d9a
이상진 <102288397+lsj0202@users.noreply.github.com>
2024-05-31 21:09:18 +0900

    feat: add header component (#2)

---
+++ src/components/Header.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { cn } from '@/utils';
+import classNames from 'classnames';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import Container from './layout/Container';
+import Wrapper from './layout/Wrapper';
+
+const Header = () => {
+  const [showShadow, setShowShadow] = useState(false);
+
+  useEffect(() => {
+    addEventListener('scroll', () => {
+      if (window.scrollY > 60) {
+        setShowShadow(true);
+      } else {
+        setShowShadow(false);
+      }
+    });
+
+    return () => {
+      removeEventListener('scroll', () => {});
+    };
+  }, []);
+
+  return (
+    <Wrapper
+      className={cn(
+        classNames(
+          'fixed transition ease-in-out bg-white',
+          showShadow && 'shadow-md ease-in-out',
+        ),
+      )}
+    >
+      <Container>
+        <header className="flex h-[60px] items-center justify-between">
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            width={150}
+            height={40}
+            className="cursor-pointer"
+          />
+          <div className="cursor-pointer">로그인</div>
+        </header>
+      </Container>
+    </Wrapper>
+  );
+};
+
+export default Header;
