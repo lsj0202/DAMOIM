b8eafd19b2e60d020a014c7f4a5309a2e5f1dd21
이상진 <102288397+lsj0202@users.noreply.github.com>
2024-06-01 15:22:59 +0900

    feat(common): add Text component, Text storybook (#9)
    * feat: add Text component, Text storybook
    
    * feat: add 리뷰 반영

---
+++ src/components/common/Text.tsx
@@ -0,0 +1,60 @@
+import { cn } from '@/utils';
+import { cva } from 'class-variance-authority';
+import classNames from 'classnames';
+import { forwardRef } from 'react';
+
+type TextProps = {
+  size?: 'xl' | 'lg' | 'md' | 'sm' | 'xs';
+  color?: 'white' | 'black' | 'orange' | 'gray';
+  weight?: 'bold' | 'semibold' | 'medium' | 'light';
+  className?: string;
+  children: React.ReactNode;
+};
+
+const TextVariants = cva('', {
+  variants: {
+    size: {
+      xl: 'text-5xl leading-[55px]',
+      lg: 'text-xl',
+      md: 'text-lg',
+      sm: 'text-base',
+      xs: 'text-sm',
+    },
+    color: {
+      white: 'text-white',
+      black: 'text-black',
+      orange: 'text-orange-500',
+      gray: 'text-slate-300',
+    },
+    weight: {
+      bold: 'font-bold',
+      semibold: 'font-semibold',
+      medium: 'font-medium',
+      light: 'font-light',
+    },
+    defaultVariants: {
+      size: 'md',
+      color: 'black',
+      weight: 'medium',
+    },
+  },
+});
+
+const Text = forwardRef(function Text(
+  { size, color, weight, className, children, ...props }: TextProps,
+  ref: React.ForwardedRef<HTMLSpanElement>,
+) {
+  return (
+    <span
+      ref={ref}
+      {...props}
+      className={cn(
+        classNames(TextVariants({ size, color, weight }), className),
+      )}
+    >
+      {children}
+    </span>
+  );
+});
+
+export default Text;
---
+++ src/stories/Text.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Text } from '@/components/common';
+
+const meta = {
+  title: 'Text',
+  component: Text,
+  tags: ['autodocs'],
+  args: {
+    children: 'Hello, World!',
+  },
+} satisfies Meta<typeof Text>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const DefaultText: Story = {
+  args: {
+    size: 'md',
+    color: 'orange',
+    weight: 'semibold',
+  },
+};
