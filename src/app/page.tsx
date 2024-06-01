b821f2be9e050cbc8406d7e26bf1afe9650bcc36
lsj0202 <w1004123@gmail.com>
2024-06-01 15:12:10 +0900

    feat: add Text component, Text storybook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Container, Header } from '@/components';
+import { Text } from '@/components/common';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import Image from 'next/image';
@@ -13,8 +14,8 @@ const Home = () => {
     gsap.from('#walking', { x: -100, duration: 1 });
 
     const TextTl = gsap.timeline();
-    TextTl.from('#main_text', { opacity: 0, y: -50, duration: 0.7 })
-      .from('#sub_text', { opacity: 0, y: -20, duration: 0.4 })
+    TextTl.from('.mainT', { opacity: 0, y: -50, duration: 0.7 })
+      .from('.subT', { opacity: 0, y: -20, duration: 0.4 })
       .from('#main_button', { opacity: 0, y: -20, duration: 0.4 });
   }, []);
 
@@ -23,13 +24,13 @@ const Home = () => {
       const itemTl = gsap.timeline();
 
       itemTl
-        .from('.item', {
+        .from('.club', {
           opacity: 0,
           y: -60,
           duration: 1,
           stagger: 0.2,
           onComplete: () => {
-            gsap.to('.item', { opacity: 1, y: 0 });
+            gsap.to('.club', { opacity: 1, y: 0 });
           },
         })
         .from('#sub_button', { opacity: 0, y: -20 });
@@ -42,16 +43,13 @@ const Home = () => {
       <div className="flex h-screen items-center justify-center bg-gradient-to-r-orange pt-[60px]">
         <Container className="flex h-[80vh] items-center">
           <div className="flex h-[400px] w-2/3 flex-col">
-            <div
-              id="main_text"
-              className="text-5xl font-semibold leading-[55px] text-white"
-            >
+            <Text className="mainT" size="xl" weight="semibold" color="white">
               쉽고, 간편하게 찾아보는 <br />
               스포츠 클럽
-            </div>
-            <div id="sub_text" className="mt-7 text-lg font-medium text-white">
+            </Text>
+            <Text className="subT mt-7" size="md" weight="medium" color="white">
               다모임을 사용하여 다양한 사람들과 다양한 스포츠를 즐겨보세요!
-            </div>
+            </Text>
             <button
               id="main_button"
               className="mt-10 w-40 rounded-lg bg-white py-4 font-semibold text-orange-500 hover:bg-slate-100"
@@ -72,20 +70,20 @@ const Home = () => {
       </div>
       <div className="bg-slate-50" style={{ minHeight: 'calc(100vh - 60px )' }}>
         <Container className="flex flex-col">
-          <div className="my-10 text-xl font-semibold">
+          <Text className="my-10" size="lg" weight="semibold">
             현재 인기있는 스포츠 클럽들을 추천해 드릴게요 🔥
-          </div>
+          </Text>
           <div className="flex flex-wrap gap-3" ref={clubRef}>
-            <div className="item h-[400px] w-[280px] rounded-lg bg-white shadow-lg">
+            <div className="club h-[400px] w-[280px] rounded-lg bg-white shadow-lg">
               인기 스포츠 클럽 영역
             </div>
-            <div className="item h-[400px] w-[280px] rounded-lg bg-white shadow-lg">
+            <div className="club h-[400px] w-[280px] rounded-lg bg-white shadow-lg">
               인기 스포츠 클럽 영역
             </div>
-            <div className="item h-[400px] w-[280px] rounded-lg bg-white shadow-lg">
+            <div className="club h-[400px] w-[280px] rounded-lg bg-white shadow-lg">
               인기 스포츠 클럽 영역
             </div>
-            <div className="item h-[400px] w-[280px] rounded-lg bg-white shadow-lg">
+            <div className="club h-[400px] w-[280px] rounded-lg bg-white shadow-lg">
               인기 스포츠 클럽 영역
             </div>
           </div>
