cc437733262b5f41cff4c8fa79af71330c65d43d
lsj0202 <w1004123@gmail.com>
2024-06-03 13:56:36 +0900

    chore: 리뷰 반영

--- src/components/common/Flex.tsx
+++ src/components/common/Flex.tsx
@@ -32,20 +32,6 @@ const FlexVariants = cva('flex', {
       col: 'flex-col',
       row: 'flex-row',
     },
-    gap: {
-      0: 'gap-0',
-      1: 'gap-1',
-      2: 'gap-2',
-      3: 'gap-3',
-      4: 'gap-4',
-      5: 'gap-5',
-      6: 'gap-6',
-      8: 'gap-8',
-      10: 'gap-10',
-      12: 'gap-12',
-      16: 'gap-16',
-      20: 'gap-20',
-    },
   },
 });
 
